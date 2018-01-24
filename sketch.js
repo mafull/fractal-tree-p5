b1b0a465008728346e3b2847110b4a869e42967d
Max Fuller <max.fuller01@btinternet.com>
2018-01-24 12:40:02 +0000

    Added option to change the number of trees that are drawn

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
+var numTrees;
 var startLength;
 var scaleFactor;
 var minLength;
 
+var numTreesInput;
 var angleSlider;
 
 function setup() {
 	createCanvas(800, 800);
+	numTreesInput = createInput("3");
+	numTreesInput.input(inputEvent);
 	angleSlider = createSlider(0, PI, (PI / 4), 0);
 
-	startLength = width / 4;
+	numTrees = 3;
+	startLength = width / 7;
 	scaleFactor = 0.67;
 	angle = angleSlider.value();
 	minLength = 4;
@@ -20,8 +25,15 @@ function draw() {
 	background(51);
 
 	stroke(255);
-	translate((width / 2), height);
-	branch(startLength);
+	translate((width / 2), (height / 2));
+
+	var treeAngle = (2 * PI) / numTrees;
+	for(var i = 0; i < numTrees; i++) {
+		push();
+		rotate(treeAngle * i);
+		branch(startLength);
+		pop();
+	}
 }
 
 
@@ -40,4 +52,10 @@ function branch(length) {
 		branch(length * scaleFactor);
 		pop();
 	}
+}
+
+
+function inputEvent() {
+	console.log(numTreesInput.value());
+	numTrees = numTreesInput.value();
 }
